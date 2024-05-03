c92858f5c9bc1a5f9a09013b8a2c2f7deafd2089
Yuriy Kozhev <kozhev.dev@gmail.com>
2024-05-03 17:46:02 +0300

    Добавлена страница Фото

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
+// CSS анимации
+
 new WOW().init();
+
+// Галерея
+
+const photo = document.querySelectorAll('.photo__item');
+
+photo.forEach(item => {
+    if (item.naturalWidth > item.naturalHeight) {
+        item.classList.add('photo__item_wide');
+    }
+})
