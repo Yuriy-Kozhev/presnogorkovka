e9a359bf973b805edf9a0c5757a12b86064dc060
Yuriy Kozhev <kozhev.dev@gmail.com>
2024-05-04 14:25:37 +0300

    Добавлен дропдаун с годами в галерее

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,24 @@ photo.forEach(item => {
         item.classList.add('photo__item_wide');
     }
 })
+
+// Выпадающий список
+
+const dropdown = document.querySelector('.btn__dropdown-content'),
+    dropbtn = document.querySelector('.btn__dropdown'),
+    droplink = document.querySelectorAll('.btn__dropdown-content')
+
+dropbtn.addEventListener('mouseenter', () => {
+    dropdown.classList.add('btn__dropdown-content_show')
+});
+
+dropbtn.addEventListener('mouseleave', () => {
+    dropdown.classList.remove('btn__dropdown-content_show')
+});
+
+droplink.forEach(item => {
+    item.addEventListener('click', () => {
+        dropdown.classList.remove('btn__dropdown-content_show')
+    })
+})
+
