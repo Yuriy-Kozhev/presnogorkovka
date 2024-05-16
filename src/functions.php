8fd2c06e2db2372920d746f9c02f68818575caef
Yuriy Kozhev <kozhev.dev@gmail.com>
2024-05-16 06:52:31 +0300

    Исправлены пути к шрифтам. Добавлен динамический пост в Творчество

--- src/functions.php
+++ src/functions.php
@@ -4,6 +4,7 @@
     function pg_scripts() {
         wp_enqueue_style('pg-style', get_stylesheet_uri());
         wp_enqueue_script('pg-script', get_template_directory_uri() . '/assets/js/script.js', array(), null, true);
+        wp_enqueue_script('pg-script-wow', get_template_directory_uri() . '/assets/js/wow.min.js', array(), null, true);
     };
  
     add_theme_support( 'post-thumbnails' );
